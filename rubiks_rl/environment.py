Fix right rotation

rubiks_rl/environment.py
@@ -63,7 +63,7 @@ class RubiksCube(gym.Env):
     def step(self, action: int) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
         self.run_action(action)
         reward = self.reward()
-        done = reward == len(Face) * 9
+        done = reward == len(Face) * (self.n_rows**2)
         step_reward = reward - self.current_reward
         self.current_reward = reward
 
@@ -82,7 +82,7 @@ class RubiksCube(gym.Env):
             if action == 0:
                 self.cube[Face.LEFT.value] = np.rot90(self.cube[Face.LEFT.value])
             if action == self.n_rows - 1:
-                self.cube[Face.TOP.value] = np.rot90(self.cube[Face.RIGHT.value])
+                self.cube[Face.RIGHT.value] = np.rot90(self.cube[Face.RIGHT.value])
             self.rotate_col(action)
 
     def render(self) -> RenderFrame | list[RenderFrame] | None:

rubiks_rl/train.py
@@ -29,7 +29,7 @@ class AttentionExtractor(BaseFeaturesExtractor):
         self.multihead_attn = torch.nn.MultiheadAttention(
             embed_dim=n_heads * n_emb, num_heads=n_heads, batch_first=True
         )
-        self.linear = torch.nn.Linear(648, features_dim)
+        self.linear = torch.nn.Linear(288, features_dim)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # Something is casting to float...ugh
@@ -64,7 +64,7 @@ def main():
         model.policy = model.policy.to(device="cuda")
 
     model.policy.compile(mode=mode)
-    model.learn(total_timesteps=2_500_000, progress_bar=True)
+    model.learn(total_timesteps=10_000, progress_bar=True)
 
     model.save("rubiks")
 
@@ -72,7 +72,7 @@ def main():
 
     model = RecurrentPPO.load("rubiks")
 
-    env = RubiksCube()
+    env = RubiksCube(shuffle=True, n_rows=args.n_rows)
     obs, _ = env.reset()
     while True:
         action, _state = model.predict(obs)